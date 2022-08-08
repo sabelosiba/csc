cbffcc812f5b946075f82c6e1a27ff533076f481
Sabelo Dlamini <dlmsab007@myuct.ac.za>
2022-08-08 22:50:15 +0200

    Mean filter serial version 1.1

--- src/MeanFilterSerial.java
+++ src/MeanFilterSerial.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.awt.Color;
+import java.io.*;
 
 public class MeanFilterSerial{
     private String inImage;
@@ -12,29 +13,37 @@ public class MeanFilterSerial{
     BufferedImage image =null;
     File f = null;
     
+    int sumRed = 0;
+    int sumGreen =0;
+    int sumBlue =0;
+    
        
     public static void main(String args[]) throws FileNotFoundException{
         MeanFilterSerial mean;
+        int wii = Integer.parseInt(args[2]);
         try{
-            if(args.length > 0)){
-                mean = new MeanFilterSerial(args[0], args[1], args[2]); 
+            if(wii >= 3 ){
+                mean = new MeanFilterSerial(args[0], args[1], wii);
                 mean.imageMean();
             }
-        }catch(Excepton e){
+            else{
+                System.out.println("Enter window width that is an odd number and atleast 3");
+            }
+        }catch(Exception e){
             System.out.println("Error somewhere");
         }
     }
     
-    public MeanFilterSerial(String inImage, String outImage, int window){
-        inImage = this.inpImage;
+    public MeanFilterSerial(String inImage, String outImage, int window) throws IOException{
+        inImage = this.inImage;
         outImage = this.outImage;
-        window = this.window
+        window = this.window;
         read();
     }
     
     public void read() throws IOException{
         try{
-            f = new File(loc + inImage + "\"");
+            f = new File("/home/sabelo/csc2/data/index.jpeg");
             image = ImageIO.read(f);
             System.out.println("Read image successful");
         }catch(IOException e){
@@ -42,15 +51,42 @@ public class MeanFilterSerial{
         }
     }
     
-    public void imageMean(){
-        
-        
+    public void imageMean() throws IOException{
+        System.out.println("WhAT");
+        int max = window*window;
+        int odd = (window - (int)(window/2)) -1;
+        int h = image.getHeight();
+        int w = image.getWidth();
+        //int[] pixelArr = new int[max];
+        for(int i=odd; i< h-odd; i++){
+            for(int j=odd; j< w-odd; j++){
+                int index =0;
+                int[] pixelArr = new int[max];
+                for(int i1= i-odd; i1<= i+odd; i1++){
+                    for(int j1= j-odd; j1<= j+odd; j1++){
+                        pixelArr[index++] = image.getRGB(j1,i1);
+                    }
+                }
+                for(int x=0; x<max; x++){
+                    sumRed += (pixelArr[x] >> 16) & 0xff;
+                    sumGreen += (pixelArr[x] >> 8) & 0xff;
+                    sumBlue += (pixelArr[x]) & 0xff; 
+                }
+                sumRed =sumRed/ max;
+                sumGreen = sumGreen/max;
+                sumBlue = sumBlue/max;
+                int mean = (sumRed<<16) | (sumGreen<<8) | sumBlue;
+                image.setRGB(j,i, (int)(mean));
+            }
+        }    
+        System.out.println("DONE");
+        printImage();
     }
     
-    public void printImage(){
+    public void printImage() throws IOException{
         try{
-            f = new File(loc + outImage + "\"");
-            image = new BufferedImage(image.Width, image.Heigth, BufferedImage.TYPE_INT_ARGB);
+            f = new File("/home/sabelo/csc2/data/inde.jpeg");
+            //image = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
             ImageIO.write(image, "jpg", f);
             System.out.println("write image complete");
         }catch(IOException e){
