813f4418934a3b2fc26a7195e81b46899d485887
Sabelo Dlamini <dlmsab007@myuct.ac.za>
2022-08-09 22:24:37 +0200

    Median serial version 1.1

--- src/MedianFilterSerial.java
+++ src/MedianFilterSerial.java
@@ -7,7 +7,7 @@ import javax.imageio.ImageIO;
 import java.io.*;
 
 public class MedianFilterSerial{
-    private int window = 0;
+    private int  window = 0;
     private String inImage = "";
     private String outImage ="";
     BufferedImage image = null;
@@ -21,21 +21,22 @@ public class MedianFilterSerial{
     int index = 0;
     int sum=0;
     
-    public MedianFilterSerial(String inImage, String outImage, int window){
+    public MedianFilterSerial(String inImage, String outImage, String wind) throws IOException{
         inImage = this.inImage;
         outImage = this.outImage;
-        window = this.window;
+        window = Integer.parseInt(wind);
         read();
         max = window*window;
         odd = (window - 1) / 2;
         pixelArr = new int[max];
         H = image.getHeight();
         W = image.getWidth();
+        System.out.println(window +"\n"+ max +"\n" + H +"\n" + W +"\n" + odd);
     }
     
-    public void read(){
+    public void read() throws IOException{
         try{
-            f = new File("/home/csc2/data/"+inImage+"\"");
+            f = new File("/home/sabelo/csc2/data/index.jpeg");
             image = ImageIO.read(f);
             System.out.println("Image read successful");
         }catch(Exception e){
@@ -44,8 +45,11 @@ public class MedianFilterSerial{
     }
     
     public void imageMedian(){
+        System.out.println("ngakifika");
+        
         for(int i=odd; i<H-odd; i++){
             for(int j=odd; j<W-odd; j++){
+                index =0;
                 for(int i1=i-odd; i1<=i+odd; i1++){
                     for(int j1=j-odd; j1<=j+odd; j1++){
                         pixelArr[index++] = image.getRGB(j1,i1);
@@ -65,7 +69,7 @@ public class MedianFilterSerial{
     
     public void writeImage(){
         try{
-            f = new File("/home/csc2/data/"+outImage+ "\"");
+            f = new File("/home/sabelo/csc2/data/"+outImage+ "\"");
             ImageIO.write(image, "jpeg", f );
             System.out.println("image write complete");
         }catch(Exception e){
@@ -74,9 +78,11 @@ public class MedianFilterSerial{
     }
     public static void main(String args[]) throws FileNotFoundException, IOException{
         int space = Integer.parseInt(args[2]);
+        System.out.println(space);
         try{
             if((space >= 3) && (args.length > 0)){
-                MedianFilterSerial median = new MedianFilterSerial(args[0], args[1], space);
+                MedianFilterSerial median = new MedianFilterSerial(args[0], args[1], args[2]);
+                System.out.println(space);
                 median.imageMedian();
             }else{
                 System.out.println("Enter correct file names and/or odd number width atleast 3");
